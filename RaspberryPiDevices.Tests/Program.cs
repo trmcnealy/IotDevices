24ea0379b13934ea83d6c81d0ee2766ce0b21aeb
Timothy McNealy <timothy.mcnealy@gmail.com>
2024-01-02 13:47:17 -0600

    Remote Febugging

--- RaspberryPiDevices.Tests/Program.cs
+++ RaspberryPiDevices.Tests/Program.cs
@@ -7,11 +7,52 @@ using System.Device.Pwm;
 using System.Device.Pwm.Drivers;
 using System.Device.Spi;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 
 using Iot.Device.Adc;
 using Iot.Device.Board;
+using Iot.Device.Card.CreditCardProcessing;
 using Iot.Device.Mfrc522;
+
+
+public static class Try
+{
+    public static void Catch(Action action)
+    {
+        try
+        {
+            action.Invoke();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"{action.Method.Name}:"+ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine($"{action.Method.Name}");
+        }
+    }
+
+    //public static object? Catch<TDelegate>(this TDelegate action) where TDelegate : Delegate
+    //{
+    //    object? obj = null;
+    //    try
+    //    {
+    //        obj = action.DynamicInvoke();
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        Console.WriteLine($"{action.Method.Name}:"+ex.Message);
+    //    }
+    //    finally
+    //    {
+    //        Console.WriteLine($"{action.Method.Name}");
+    //    }
+    //    return obj;
+    //}
+}
+
 
 namespace RaspberryPiDevices.Tests
 {
@@ -19,121 +60,57 @@ namespace RaspberryPiDevices.Tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Raspberry Pi Devices Tests");
 
-            IOControllerTests tests = new IOControllerTests();
-
-
-
-            tests.TestMethod1();
-            tests.InputPullResistorsWork();
-            tests.OpenPinDefaultsModeToLastModeIncludingPulls();
-            tests.HighPulledPinDoesNotChangeToLowWhenChangedToOutput();
-            tests.PinCountReportedCorrectly();
-            tests.OpenTwiceGpioPinAndClosedTwiceThrows();
-            tests.WriteInputPinDoesNotThrow();
-            tests.GpioControllerCreateOpenClosePin();
-            tests.IsPinModeSupported();
-            tests.GetPinMode();
-            tests.UsingBoardNumberingWorks();
-            tests.UsingLogicalNumberingDisposesTheRightPin();
-            tests.UsingBoardNumberingDisposesTheRightPin();
-            tests.CallbackOnEventWorks();
-            tests.WriteSpan();
-            tests.ReadSpan();
-            //public async Task WaitForEventAsyncFail();
-            tests.WaitForEventSuccess();
-            //private static TValue IsAny<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]TValue>();
-
+            Console.WriteLine($"{nameof(IsRaspi4)}={IsRaspi4()}");
+           
 
             RaspberryPiBoard raspberryPiBoard = new RaspberryPiBoard();
             GpioController gpioController = raspberryPiBoard.CreateGpioController();
 
 
-            int busId = 0;
-            int pinNumber = 0;
-            int channel = 0;
-            int chip = 1;
-            int pwmChannel = 0;
-            int i2cAddress = 0;
 
-            SpiConnectionSettings connectionSettings = new SpiConnectionSettings(busId, chip);
+            //PinUsage.AnalogOut
+
+            SpiConnectionSettings connectionSettings = new SpiConnectionSettings(24, 0);
             SpiDevice spi = (raspberryPiBoard is RaspberryPiBoard) ? SpiDevice.Create(connectionSettings) : raspberryPiBoard.CreateSpiDevice(connectionSettings);
-            bool SpiActivated = raspberryPiBoard.IsSpiActivated();
+
+            bool spiActivated = raspberryPiBoard.IsSpiActivated();
+            Console.WriteLine($"{nameof(spiActivated)}={spiActivated}");
 
             int[] pinAssignmentSpi = raspberryPiBoard.GetDefaultPinAssignmentForSpi(connectionSettings);
-            int[] OverlayPinAssignmentForSpi = raspberryPiBoard.GetOverlayPinAssignmentForSpi(connectionSettings);
+            foreach(int pinAssignment in pinAssignmentSpi)
+            {                
+                Console.WriteLine($"{nameof(pinAssignment)}={pinAssignment}");
+            }
 
-            Assert.AreEqual(9, pinAssignmentSpi[0]);
-            Assert.AreEqual(10, pinAssignmentSpi[1]);
-            Assert.AreEqual(11, pinAssignmentSpi[2]);
-
-
-
-            I2cConnectionSettings settings = new I2cConnectionSettings(raspberryPiBoard.GetDefaultI2cBusNumber(), i2cAddress);
-            I2cDevice i2c = raspberryPiBoard.CreateI2cDevice(settings);
-
-            bool I2cActivated = raspberryPiBoard.IsI2cActivated();
-            int I2cBusNumber = raspberryPiBoard.GetDefaultI2cBusNumber();
-            int[] pinAssignmentI2c = raspberryPiBoard.GetDefaultPinAssignmentForI2c(busId);
-            int[] OverlayPinAssignmentForI2c = raspberryPiBoard.GetOverlayPinAssignmentForI2c(busId);
-
-            Assert.AreEqual(0, pinAssignmentI2c[0]);
-            Assert.AreEqual(1, pinAssignmentI2c[1]);
-
-            busId = 1;
-            pinAssignmentI2c = raspberryPiBoard.GetDefaultPinAssignmentForI2c(busId);
-            Assert.AreEqual(2, pinAssignmentI2c[0]);
-            Assert.AreEqual(3, pinAssignmentI2c[1]);
-
-
-            int PinAssignmentForPwm = raspberryPiBoard.GetDefaultPinAssignmentForPwm(chip, channel);
-            int OverlayPinAssignmentForPwm = raspberryPiBoard.GetOverlayPinAssignmentForPwm(pwmChannel);
-
-            using (PwmChannel pwm = CreatePwmChannel(300))
-            {
-                Assert.AreEqual(300, pwm.Frequency);
-
-                pwm.Frequency = 100;
-                Assert.AreEqual(100, pwm.Frequency);
-
-                pwm.Frequency = 200;
-                Assert.AreEqual(200, pwm.Frequency);
-
-                pwm.Frequency = 500;
-                Assert.AreEqual(500, pwm.Frequency);
+            int[] overlayPinAssignmentForSpi = raspberryPiBoard.GetOverlayPinAssignmentForSpi(connectionSettings);
+            foreach(int overlayPinAssignment in overlayPinAssignmentForSpi)
+            {                
+                Console.WriteLine($"{nameof(overlayPinAssignment)}={overlayPinAssignment}");
             }
 
 
 
-            PinUsage CurrentPinUsage = raspberryPiBoard.DetermineCurrentPinUsage(pinNumber);
 
-            ComponentInformation QueryComponentInformation = raspberryPiBoard.QueryComponentInformation();
 
 
             Console.ReadKey();
         }
 
-
-        public static PwmChannel CreatePwmChannel(int frequency = 10000, bool stopped = false, double dutyCycle = 0.5)
+        static bool IsRaspi4()
         {
-            PwmChannel pwm;
+            if (File.Exists("/proc/device-tree/model"))
+            {
+                string model = File.ReadAllText("/proc/device-tree/model", Encoding.ASCII);
 
-            try
-            {
-                pwm = PwmChannel.Create(0, 0, frequency, dutyCycle);
-            }
-            catch (ArgumentException) when (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                pwm = new SoftwarePwmChannel(18, frequency, dutyCycle, usePrecisionTimer: true);
-            }
-
-            if (!stopped)
-            {
-                pwm.Start();
+                if (model.Contains("Raspberry Pi 4") || model.Contains("Raspberry Pi Compute Module 4"))
+                {
+                    return true;
+                }
             }
 
-            return pwm;
+            return false;
         }
     }
 }
--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
+clear
 # dotnet build RaspberryPiDevices/RaspberryPiDevices.csproj --configuration Debug --framework net8.0 --runtime linux-arm64 --self-contained false --output ./trmcnealy/Tests/bin/
 #dotnet build RaspberryPiDevices.Tests/RaspberryPiDevices.Tests.csproj --no-restore --configuration Debug --framework net8.0 --runtime linux-arm64 --self-contained false
 
 #cd ~/Tests/IotDevices/RaspberryPiDevices.Tests/bin/Debug/net8.0/linux-arm64/
 cd ~/Tests/IotDevices/
 
-dotnet build RaspberryPiDevices.Tests/RaspberryPiDevices.Tests.csproj --no-restore --configuration Debug --framework net8.0 --runtime linux-arm64 --self-contained false --use-current-runtime false
+#dotnet restore
 
-dotnet run --no-restore --project /RaspberryPiDevices.TestsRaspberryPiDevices.Tests.csproj --property:Configuration=Debug --property:Framework=net8.0 -property:Runtime=linux-arm64
+#dotnet build RaspberryPiDevices.Tests/RaspberryPiDevices.Tests.csproj --no-restore --configuration Debug --framework net8.0 --runtime linux-arm64 --self-contained false --use-current-runtime false
+#dotnet build RaspberryPiDevices.Tests/RaspberryPiDevices.Tests.csproj --no-restore --configuration Debug --framework net8.0 --arch arm64 --os linux --self-contained false
+
+dotnet run --no-restore --project RaspberryPiDevices.Tests/RaspberryPiDevices.Tests.csproj --property:Configuration=Debug
 
 #dotnet run RaspberryPiDevices.Tests.dll
 
